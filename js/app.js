61449405498ee7addb0107007c59d7f177eec79b
v <vivyanw@uw.edu>
2014-11-25 10:13:36 -0800

    fixing angular and stuff to work together

--- js/app.js
+++ js/app.js
@@ -13,6 +13,7 @@ angular.module('CommentApp', [])
     .controller('CommentController', function($scope, $http) {
     	//get or refresh all task objects saved by my application on Parse.com
         $scope.refreshComments = function() {
+            console.log("line 16 reached");
             $scope.loading = true;
             $http.get('https://api.parse.com/1/classes/comments')
                 .success(function(responseData) {
@@ -20,6 +21,7 @@ angular.module('CommentApp', [])
                     //return an object with one property called results
                     //which will can an array with all the data objects
                     $scope.comments = responseData.results;
+                    console.log("line 24 worked");
                     console.log($scope.comments);
                 })
                 .error(function(err) {
@@ -27,38 +29,50 @@ angular.module('CommentApp', [])
                     //notify user in some way
                 })
                 .finally(function() {
+                    console.log("finally worked");
                     $scope.loading = false;
                 });
         }; //scope.refresh comments
 
+
         //call the refreshComments() to get the initial set of comments on the page load
-        $scope.refreshTasks();
+        $scope.refreshComments();
 
         //initialize a new comment object on the scope for the new comment form
-        $scope.newComment = {done: false};
+        $scope.comment = {done: false};
 
         //function to add a new comment to the list
         $scope.addComment = function(comment) {
-
+            console.log("line 43 reached");
+            console.log($scope.comment);
             //post will add (insert) a new item to the class
-            $http.post('https://api.parse.com/1/classes/comments', comment)
+            $http.post('https://api.parse.com/1/classes/comments', $scope.comment)
                 .success(function(responseData) {
                     //parse.com will return the new objectId in the response data
                     //copy that to the comment we just inserted
-                    comment.objectId = responseData.objectId;
+                    $scope.comment.objectId = responseData.objectId;
+                    console.log("reached setting comment to response data");
 
                     //add that comment to our comment list
-                    $scope.comments.push(comment);
+                    $scope.comments.push($scope.comment);
+                    console.log("pushed on the comment");
 
                     //reset the newComment clear the form
-                    $scope.newComment = {done: false};
+                    $scope.comment = {done: false};
+                    console.log("new comment set to active");
+                })
+                .error(function(err) {
+                    console.log(err);
                 });
         };
 
         //function to update an exisiting taks
         $scope.updateComment = function(comment) {
+            console.log("line 67 reached");
             $scope.updating = true;
-            $http.put('https://api.parse.com/1/classes/comments'+ comments.objectId, comment)
+            console.log("comment:");
+            console.log(comment);
+            $http.put('https://api.parse.com/1/classes/comments'+ $scope.comment.objectId, comment)
                 .success(function(responseData) {
                     //don't need to do anything because the local object is already up-to-date
                 })
@@ -68,6 +82,22 @@ angular.module('CommentApp', [])
                 })
                 .finally(function() {
                     $scope.updating = false;
+                });
+        };
+
+        $scope.removeComment = function(comment) {
+            console.log("comment");
+            console.log(comment);
+
+            $http.delete('https://api.parse.com/1/classes/comments'+ $scope.comment.objectId, comment)
+                .success(function() {
+                    console.log("comment deleted");
+                })
+                .error(function(err) {
+                    console.log(err);
+                })
+                .finally(function() {
+                    console.log("ran removeComment function");
                 });
         };
 
